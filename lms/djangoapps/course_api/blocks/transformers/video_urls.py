37598e27cf09f4537da434b5b696546f85058950
DawoudSheraz <dawoud.sheraz@arbisoft.com>
2019-09-16 15:14:41 +0500

    update transformer

--- lms/djangoapps/course_api/blocks/transformers/video_urls.py
+++ lms/djangoapps/course_api/blocks/transformers/video_urls.py
@@ -11,6 +11,10 @@ from openedx.core.djangoapps.content.block_structure.transformer import BlockStr
 
 
 class VideoBlockURLTransformer(BlockStructureTransformer):
+    """
+    Transformer to re-write video urls for the mobile applications
+    to server content from edx-video.
+    """
 
     @classmethod
     def name(cls):
@@ -24,16 +28,25 @@ class VideoBlockURLTransformer(BlockStructureTransformer):
 
     @classmethod
     def collect(cls, block_structure):
-        pass
-
-    def transform(self, usage_info, block_structure):
-
+        """
+        collect video block's student view data.
+        """
         for block_key in block_structure:
             if block_key.block_type != 'video':
                 continue
-            student_view_data = block_structure.get_xblock_field(block_key, 'student_view_data')
+            xblock = block_structure.get_xblock(block_key)
+            block_structure.set_transformer_block_field(block_key, cls, 'student_view_data', xblock.student_view_data())
+
+    def transform(self, usage_info, block_structure_block_data):
+        """
+        Re-write all the video blocks' encoded videos URLs.
+        """
+        for block_key in block_structure_block_data:
+            if block_key.block_type != 'video':
+                continue
+            student_view_data = block_structure_block_data.get_xblock_field(block_key, 'student_view_data')
             encoded_videos = student_view_data['encoded_videos']
             for video_data in six.itervalues(encoded_videos):
                 video_data['url'] = rewrite_video_url(self.cdn_url, video_data['url'])
 
-            block_structure.set_transformer_block_field(block_key, self, 'student_view_data', student_view_data)
+            block_structure_block_data.set_transformer_block_field(block_key, self, 'student_view_data', {})
