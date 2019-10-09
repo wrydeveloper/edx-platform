282d128763ca23b3fd19bb74d2d07742fc69fcf5
wrydeveloper <13047908816@163.com>
2019-10-09 10:54:53 +0800

    [ADD]新增钉钉第三方登录功能

--- common/djangoapps/third_party_auth/dingtalk.py
+++ common/djangoapps/third_party_auth/dingtalk.py
@@ -1,11 +1,12 @@
 import time
 import base64
 import hmac
+import json
+import urllib
 from hashlib import sha256
 from social_core.backends.oauth import BaseOAuth2
 
 class DingtalkOAuth2(BaseOAuth2):
-    "钉钉　并不是oAuth2的接入"
     name = "dingtalk"
     AUTHORIZATION_URL = 'https://oapi.dingtalk.com/connect/qrconnect'
     ACCESS_TOKEN_URL = ''
@@ -13,7 +14,6 @@ class DingtalkOAuth2(BaseOAuth2):
     def auth_params(self, *args, **kwargs):
         params = super(DingtalkOAuth2, self).auth_params(*args, **kwargs)
         params['appid'] = params.pop('client_id')
-        params['state'] = 'STATE'
         params['scope'] = 'snsapi_login'
         return params
 
@@ -22,18 +22,22 @@ class DingtalkOAuth2(BaseOAuth2):
 
     def user_data(self, access_token, *args, **kwargs):
         appid, appsecret = self.get_key_and_secret()
-        now_timestamp = int(round(time.time()* 1000))
-        signature = base64.b64encode(hmac.new(appsecret, now_timestamp, digestmod=sha256).digest())
-        userinfo_url = 'https://oapi.dingtalk.com/sns/getuserinfo_bycode?\
-                        accessKey={accessKey}&timestamp={timestamp}&signature={signature}'\
-                        .format(accessKey=appid, timestamp=now_timestamp, signature=signature)
-        return self.get_json(userinfo_url, method='post',
-                             params={'tmp_auth_code': access_token})
+        now_timestamp = int(round(time.time() * 1000))
+        signature = base64.b64encode(hmac.new(str(appsecret), str(now_timestamp), digestmod=sha256).digest())
+        user_info_params_dict = {}
+        user_info_params_dict['accessKey'] = appid
+        user_info_params_dict['timestamp'] = now_timestamp
+        user_info_params_dict['signature'] = signature
+        user_info_params = urllib.urlencode(user_info_params_dict)
+        userinfo_url = 'https://oapi.dingtalk.com/sns/getuserinfo_bycode?{params}'.format(params=user_info_params)
+        params = json.dumps({'tmp_auth_code': str(access_token)})
+        headers = {'Content-Type': 'application/json'}
+        return self.get_json(userinfo_url, method='post', headers=headers, verify=False, data=params)
 
     def get_user_details(self, response):
         info = response.get('user_info')
         return {
-            "username": info['nick']
+            "username": info['nick'].encode('utf-8')
         }
 
     def get_user_id(self, details, response):
